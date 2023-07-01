83387c4df617ad0903c8de1515235544dd3264b3
meghamalviya5 <megha.malviya444@gmail.com>
2023-07-01 22:07:58 +0530

    changes---

--- src/pages/RecipeDetails/RecipeDetails.jsx
+++ src/pages/RecipeDetails/RecipeDetails.jsx
@@ -6,13 +6,16 @@ const RecipeDetails = () => {
   const { recipeId } = useParams();
   const { state } = useContext(RecipeContext);
 
+  console.log("recipe id-- ", recipeId);
+
   const foundRecipe = state.allRecipies.find(
-    (recipe) => recipe.id === recipeId
+    (recipe) => recipe.id === Number(recipeId)
   );
   const { image, name, ingredients, cuisine, instructions } = foundRecipe;
 
   return (
     <div className="recipe-details">
+      <h2>Recipe Details</h2>
       <img src={image} alt="recipe-pic" />
       <p>{name}</p>
       <p>Cuisine Type: {cuisine}</p>
